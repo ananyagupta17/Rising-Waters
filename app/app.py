f16462bdc339dd3845564417ce4a3e4af2b4bd14
ananyagupta17 <139058432+ananyagupta17@users.noreply.github.com>
2025-06-20 12:30:30 +0530

    Add Logistic regression pipeline

--- app/app.py
+++ app/app.py
@@ -3,7 +3,9 @@ import numpy as np
 import pickle  
 
 app = Flask(__name__)
-model = pickle.load(open('models/xgb_model.pkl', 'rb'))  
+
+model = pickle.load(open('models/logreg_pipeline.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
@@ -17,27 +19,30 @@ def intro():
 def predict():
     if request.method == 'POST':
         try:
+           
             features = [
-            float(request.form['temp']),
-            float(request.form['humidity']),
-            float(request.form['cloud_cover']),
-            float(request.form['annual_rainfall']),
-            float(request.form['rain_janfeb']),
-            float(request.form['rain_mar_may']),
-            float(request.form['rain_jun_sep']),
-            float(request.form['rain_octdec']),
-            float(request.form['avg_june']),
-            float(request.form['sub'])
-        ]
-            final_input = np.array([features]).reshape(1, -1)
+                float(request.form['temp']),
+                float(request.form['humidity']),
+                float(request.form['cloud_cover']),
+                float(request.form['annual_rainfall']),
+                float(request.form['rain_janfeb']),
+                float(request.form['rain_mar_may']),
+                float(request.form['rain_jun_sep']),
+                float(request.form['rain_octdec']),
+                float(request.form['avg_june']),
+                float(request.form['sub'])
+            ]
 
+            final_input = np.array([features])  
+
+           
             probability = model.predict_proba(final_input)[0][1]
-            percent = round(probability * 100, 2)  
+            percent = round(probability * 100, 2)
 
             print(final_input)
-            print(model.predict_proba(final_input))
+            print(f"Predicted probability: {percent}%")
 
-
+            
             if percent <= 50:
                 return render_template('low.html', percent=percent)
             elif percent <= 75:
@@ -47,8 +52,8 @@ def predict():
 
         except Exception as e:
             return f"Error: {e}"
+            
     return render_template('predict.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
